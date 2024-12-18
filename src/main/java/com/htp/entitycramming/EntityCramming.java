ea77831439520fb8c1242eb7b369bd99bb039431
TheBlueEyeOfEnder <wilkinskyle18@outlook.com>
2024-12-18 15:49:54 -0500

    I've finally resolved the bug where players were dying upon reaching the entity cramming cap. The logic has been updated, so players are no longer added to the entity list.

--- src/main/java/com/htp/entitycramming/EntityCramming.java
+++ src/main/java/com/htp/entitycramming/EntityCramming.java
@@ -1,5 +1,7 @@
 package com.htp.entitycramming;
 
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,7 +13,7 @@ public class EntityCramming
     public static final Logger LOGGER = LogManager.getLogger(EntityCramming.class);
     public static final String MOD_ID = "entitycramming";
     public static final String NAME = "HTP Entitycramming";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     public EntityCramming() {
     }
--- src/main/java/com/htp/entitycramming/mixin/MixinLivingEntity.java
+++ src/main/java/com/htp/entitycramming/mixin/MixinLivingEntity.java
@@ -4,6 +4,7 @@ package com.htp.entitycramming.mixin;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,23 +16,27 @@ import java.util.List;
 @Mixin(LivingEntity.class)
 public class MixinLivingEntity {
 
-    @Inject(method = "pushEntities", at = @At("HEAD"))
+    @Inject(method = "pushEntities", at = @At("HEAD"), cancellable = true)
     private void pushEntities(CallbackInfo ci) {
         LivingEntity mob = (LivingEntity) (Object) this;
+
+        if (mob instanceof Player) {
+            return;
+        }
+
         List<Entity> list = mob.level.getEntities(mob, mob.getBoundingBox(), EntitySelector.pushableBy(mob));
+
         if (!list.isEmpty()) {
-            int i = mob.level.getGameRules().getInt(GameRules.RULE_MAX_ENTITY_CRAMMING);
-            if (i > 0 && list.size() > i - 1 && mob.getRandom().nextInt(4) == 0) {
-                int j = 0;
-                for (Entity entity : list) {
-                    if (!entity.isPassenger()) {
-                        ++j;
-                    }
-                }
-                if (j > i - 1) {
-                    mob.kill(); // Kill the Entity instead of inflicting suffocation damage as asked by the commissioner.
+            int maxCramming = mob.level.getGameRules().getInt(GameRules.RULE_MAX_ENTITY_CRAMMING);
+            if (maxCramming > 0 && list.size() > maxCramming - 1 && mob.getRandom().nextInt(4) == 0) {
+                long crammedEntities = list.stream().filter(entity -> !entity.isPassenger()).count();
+                if (crammedEntities > maxCramming - 1) {
+                    mob.kill(); // Kill the Mob instantly rather than suffocation damage as requested by the Commissioner.
                 }
             }
         }
+        ci.cancel();
     }
 }
+
+
